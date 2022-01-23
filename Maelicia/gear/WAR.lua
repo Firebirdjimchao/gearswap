2399ac598ee5cde50155ca997f74a497c217b328
Firebird.jim.chao <dgunther2@hotmail.com>
2022-01-22 23:57:12 -0500

    Default binding comments for easy reference

--- Maelicia/gear/WAR.lua
+++ Maelicia/gear/WAR.lua
@@ -23,6 +23,25 @@ function user_setup()
 	update_melee_groups()
 	select_default_macro_book()
 
+	-------------------------------------------------
+	-- Default bindings
+	--
+	-- F9 - Cycle Offense Mode (the offensive half of all 'hybrid' melee modes).
+	-- Ctrl-F9 - Cycle Hybrid Mode (the defensive half of all 'hybrid' melee modes).
+	-- Alt-F9 - Cycle Ranged Mode.
+	-- Win-F9 - Cycle Weaponskill Mode.
+	-- F10 - Activate emergency Physical Defense Mode. Replaces Magical Defense Mode, if that was active.
+	-- F11 - Activate emergency Magical Defense Mode. Replaces Physical Defense Mode, if that was active.
+	-- Ctrl-F10 - Cycle type of Physical Defense Mode in use.
+	-- Alt-F12 - Turns off any emergency defense mode.
+	-- Alt-F10 - Toggles Kiting Mode.
+	-- Ctrl-F11 - Cycle Casting Mode.
+	-- F12 - Update currently equipped gear, and report current status.
+	-- Ctrl-F12 - Cycle Idle Mode.
+	-------------------------------------------------
+
+	-- "CTRL: ^ ALT: ! Windows Key: @ Apps Key: #"
+
 	send_command('bind @` gs c cycle HasteMode')
 	send_command('bind ^` gs equip sets.Twilight; input /echo --- Twilight Set Equipped ---')
 	send_command('bind ^- gs c cycle enmitymode')
