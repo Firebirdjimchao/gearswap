7cef8c57102984425613d557ab7112244ad51dd9
Firebird.jim.chao <dgunther2@hotmail.com>
2022-07-09 20:12:24 -0400

    Syntax fixes

--- Lenare/gear/WHM.lua
+++ Lenare/gear/WHM.lua
@@ -457,7 +457,7 @@ function init_gear_sets()
 	})
 
 	sets.midcast.Aquaveil = set_combine(sets.midcast['Enhancing Magic'],{
-		main="Vadose Rod"
+		main="Vadose Rod",
 		--head="Chironic Hat",
 		hands="Regal Cuffs",
 	})
